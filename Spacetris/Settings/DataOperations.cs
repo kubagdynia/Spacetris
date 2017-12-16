924418617fc4f38ef1dfb9af71b83611d3c1f297
Jakub Kurłowicz <kubagdynia@gmail.com>
2017-12-16 15:51:38 +0100

    Added GameSettings

---
+++ Spacetris/Settings/DataOperations.cs
@@ -0,0 +1,98 @@
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+
+namespace Spacetris.Settings
+{
+    public static class DataOperations
+    {
+        public static void SaveData<T>(T data, string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+            
+            string tempPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+
+            // Convert To Json then to bytes
+            string jsonData = JsonConvert.SerializeObject(data, Formatting.Indented);
+            byte[] jsonByte = Encoding.UTF8.GetBytes(jsonData);
+
+            // Create Directory if it does not exist
+            if (!Directory.Exists(Path.GetDirectoryName(tempPath)))
+            {
+                Directory.CreateDirectory(Path.GetDirectoryName(tempPath));
+            }
+
+            try
+            {
+                File.WriteAllBytes(tempPath, jsonByte);
+#if DEBUG
+                Console.WriteLine("Saved Data to: " + tempPath);
+#endif
+            }
+            catch (Exception e)
+            {
+#if DEBUG
+                Console.WriteLine("Failed to save data to: " + tempPath);
+                Console.WriteLine("Error: " + e.Message);
+#endif
+                throw;
+            }
+        }
+
+        public static T LoadData<T>(string fileName)
+        {
+            if (string.IsNullOrWhiteSpace(fileName))
+            {
+                throw new ArgumentNullException(nameof(fileName));
+            }
+            
+            string tempPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName);
+
+            // Exit if Directory or File does not exist
+            if (!Directory.Exists(Path.GetDirectoryName(tempPath)))
+            {
+#if DEBUG
+                Console.WriteLine("Directory does not exist");
+#endif
+                return default(T);
+            }
+
+            if (!File.Exists(tempPath))
+            {
+#if DEBUG
+                Console.WriteLine("File does not exist");
+#endif
+                return default(T);
+            }
+
+            // Load saved Json
+            byte[] jsonByte;
+            try
+            {
+                jsonByte = File.ReadAllBytes(tempPath);
+#if DEBUG
+                Console.WriteLine("Loaded data from: " + tempPath);
+#endif
+            }
+            catch (Exception e)
+            {
+#if DEBUG
+                Console.WriteLine("Failed to load data from: " + tempPath);
+                Console.WriteLine("Error: " + e.Message);
+#endif
+                throw;
+            }
+
+            // Convert to json string
+            string jsonData = Encoding.ASCII.GetString(jsonByte);
+
+            // Convert to Object
+            object resultValue = JsonConvert.DeserializeObject<T>(jsonData);
+            return (T)Convert.ChangeType(resultValue, typeof(T));
+        }
+    }
+}
---
+++ Spacetris/Settings/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Spacetris.Settings
+{
+    public class Settings
+    {
+        public bool IsMusic { get; set; } = true;
+        public bool IsSound { get; set; } = true;
+        public byte MusicVolume { get; set; } = 50;
+        public byte SoundVolume { get; set; } = 50;
+        public List<ScoreLine> Scores { get; set; } = new List<ScoreLine>();
+        public string FontsPath { get; set; } = "Content/Fonts/";
+        public string TilesetsPath { get; set; } = "Content/Tilesets/";
+        public string BackgroundPath { get; set; } = "Content/Background/";
+        public string SoundsPath { get; set; } = "Content/Sounds/";
+        public string MusicPath { get; set; } = "Content/Music/";
+
+    }
+}
