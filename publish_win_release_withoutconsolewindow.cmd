81d1e202b08661db3b856bb728437a9e1c552cbc
kubagdynia <kubagdynia@gmail.com>
2024-06-14 09:24:10 +0200

    Changes in project configuration

--- publish_win_release_withoutconsolewindow.cmd
+++ publish_win_release_withoutconsolewindow.cmd
@@ -1,5 +1,5 @@
 dotnet publish -c release
 
 rem removing the console window
-.\Tools\editbin.exe /subsystem:windows .\Spacetris\bin\Release\net7.0\Spacetris.exe
-.\Tools\editbin.exe /subsystem:windows .\Spacetris\bin\Release\net7.0\publish\Spacetris.exe
+.\Tools\editbin.exe /subsystem:windows .\Spacetris\bin\Release\net8.0\Spacetris.exe
+.\Tools\editbin.exe /subsystem:windows .\Spacetris\bin\Release\net8.0\publish\Spacetris.exe
